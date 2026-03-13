37e78db0d3728a7224dca89ffdbdfa64c1911ff5
SitcomReality <SitcomReality@websim.ai>
2026-03-13 06:16:37 +0000

    v5: Redesign <nav> to create a drop-down menu with sub-menus and sub-sub-menus, to support the below site layout: * Menu ** Act I *** Chapter 1 **** Alternate beginning *** Chapter 2 ** Act II *** Chapter 3 *** Chapter 4 **** Alternate ending

--- js/components/Icons.js
+++ js/components/Icons.js
@@ -13,5 +13,14 @@ export const Icons = {
   `,
   Info: () => `
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><line x1="12" y1="16" x2="12" y2="12"/><line x1="12" y1="8" x2="12.01" y2="8"/></svg>
+  `,
+  ChevronRight: () => `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+  `,
+  ChevronDown: () => `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>
+  `,
+  Menu: () => `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
   `
 };
